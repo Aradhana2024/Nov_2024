71c368b30177547eab581fce53d0a762689614c3
Aradhana2024 <aradhanasahoo27@gmail.com>
2025-01-15 19:32:11 -0500

    GET() & SET() Method

---
+++ presentation/get_set/src/AccessData.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+class Student {
+    private String name;
+    private int age;
+    Student(){
+        this.name = "Rama";
+        this.age = 29;
+    }
+    Student(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+    public void display() {
+        System.out.println("name: "+this.name);
+        System.out.println("age: "+this.age);
+    }
+}
+public class AccessData{
+    public static void main(String args[]) {
+        //Reading values from user
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the name of the student: ");
+        String name = sc.nextLine();
+        System.out.println("Enter the age of the student: ");
+        int age = sc.nextInt();
+        Student obj1 = new Student(name, age);
+        obj1.display();
+        Student obj2 = new Student();
+        obj2.display();
+    }
+}
