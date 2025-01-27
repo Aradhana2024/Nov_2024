2ccfcf2b0d5a36aa58b5b5a740ce33ef9631b70d
Aradhana2024 <aradhanasahoo27@gmail.com>
2025-01-27 12:24:47 -0500

    Array_List

---
+++ src/jan27_practice/Test.java
@@ -0,0 +1,27 @@
+package jan27_practice;
+import java.util.ArrayList;
+
+public class Test {
+    public static void main(String[] args){
+
+    ArrayList<Integer> List1 = new ArrayList<>();
+        List1.add(10);
+        List1.add(20);
+        List1.add(30);
+        List1.add(40);
+
+        System.out.println("List1 numbers are : " +List1);
+
+        ArrayList<Integer> List2 =new ArrayList<>();
+        List2.add(100);
+        List2.add(200);
+        List2.add(200);
+        List2.add(300);
+
+        System.out.println("**********");
+
+        System.out.println("List2 Numbers are " +List2);
+
+
+    }
+}
